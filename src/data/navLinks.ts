3bc3e002fd10c14bf75af36689c623094516dc82
Javier <javier.retamal.f@mail.pucv.cl>
2026-03-11 11:17:37 -0300

    Colegio

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -1,6 +1,7 @@
 export const navLinks = [
     { href: '/#inicio', label: 'Inicio', isAnchor: true },
     { href: '/beneficios', label: 'Beneficios', isAnchor: false },
+    { href: '/concurso-2026', label: 'Concurso 2026', isAnchor: false },
     { href: '/#nosotros', label: 'Nosotros', isAnchor: true },
     { href: '/#actividades', label: 'Actividades', isAnchor: true },
     { href: '/#directiva', label: 'Directiva', isAnchor: true },
